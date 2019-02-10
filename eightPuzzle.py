2073b0c3f800d1a5c6fb5671718dfdeaee3ce1f0
Robert Colvin <rcdc008@yahoo.com>
2019-02-09 16:31:16 -0800

    2/9/19 the fun part is done. need to match output to specs and keep track of stats

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -11,13 +11,18 @@ defaultPuzzlez = [[1,2,3,4,5,6,7,0,8],
 		  [1,3,6,4,5,2,7,0,8],
 		  [4,1,3,0,2,6,7,5,8],
 		  [8,6,7,2,5,4,3,0,1]] #<- this is is at diameter depth (31)
+
+#for comparison to goal state
 goal = [1,2,3,4,5,6,7,8,0]
+
+#to make priority queue FIFO when there is a tie on priority number
 tiebreaker = itertools.count()
 
 def main():
 	#prompt user for puzzle choice and store decision
 	print("Welcome to Robert Colvin's 8-puzzle solver.")
 	puzChoice = input('Type "1" to use a default puzzle, or "2" to enter your own puzzle.\n')
+	#randomly pick one of five default puzzles of varying difficulty
 	if puzChoice == "1":
 		puzzle = defaultPuzzlez[random.randint(0,4)]
 	#if user wants to create puzzle, call function for that
@@ -25,119 +30,164 @@ def main():
 		puzzle = userGeneratePuzzle()
 	else:
 		print("Invalid input. Default puzzle inbound.\n")
-		puzzle = defaultPuzzlez[random.randint(0,6)]
+		puzzle = defaultPuzzlez[random.randint(0,4)]
 	#prompt for algorithm choice
 	print("Now, choose your algorithm to solve")
 	print("1. Uniform Cost Search")
 	print("2. A* with Misplaced Tile heuristic")
 	print("3. A* with Manhattan distance heuristic")
+	#store algorithm decision
 	algChoice = input()
 	
 	if algChoice == "1":
-		generalSearch(puzzle, uniformCost)	
+		generalSearch(puzzle, algChoice)	
 	elif algChoice == "2":
-		generalSearch(puzzle, misplacedTile)
+		generalSearch(puzzle, algChoice)
 	elif algChoice == "3":
-		generalSearch(puzzle, manhattanDist)
+		generalSearch(puzzle, algChoice)
 	else:
 		print("invalid input, uniform cost search inbound\n")
-		generalSearch(puzzle, uniformCost)	
-#	for p in expand((0,puzzle)):
-#		printPuzzle(p)
+		generalSearch(puzzle, uniformCost, "1")	
 	return
-def getBlankIndex(p):
+
+#finds where blank is in list
+def getBlankIndex(p): 
 	for i in range(len(p)):
 		if p[i] == 0:
 			return i
 
+#takes a puzzle state (list) as parameter, checks if operation can be performed on that state and returns (state after operation, cost of operator) if so
+#returns original state if operation is not possible
 def moveBlankUp(p):
+	#set cost in operator functions to keep it general
+	cost = 1
 	#first check if the blank can be moved up on the simulated square game board
 	blankIndex = getBlankIndex(p)
 	#if moving the blank up would put it above the board, return the current puzzle state
 	if (blankIndex - 3) < 0:
-		return p
+		return (p,cost)
 	#else, return a copy of the board where the blank (0) has been swapped with the number above it
 	newPuzzle = p.copy()
 	newPuzzle[blankIndex] = newPuzzle[blankIndex-3]
 	newPuzzle[blankIndex-3] = 0
-	return newPuzzle
+	return (newPuzzle, cost)
 	#logic is the same for other moveBlank operators
 
 def moveBlankDown(p):
+	cost = 1
 	blankIndex = getBlankIndex(p)
 	if (blankIndex + 3) >= len(p):
-		return p
-	blankIndex = getBlankIndex(p)
-	
+		return (p,cost)
+	blankIndex = getBlankIndex(p)	
 	newPuzzle = p.copy()
 	newPuzzle[blankIndex] = newPuzzle[blankIndex+3]
 	newPuzzle[blankIndex+3] = 0
-	return newPuzzle
+	return (newPuzzle, cost)
 
 def moveBlankRight(p):
+	cost = 1
 	blankIndex = getBlankIndex(p)
 	if (blankIndex % 3) == 2:
-		return p
+		return (p,cost)
 	blankIndex = getBlankIndex(p)
 	newPuzzle = p.copy()
 	newPuzzle[blankIndex] = newPuzzle[blankIndex+1]
 	newPuzzle[blankIndex+1] = 0
-	return newPuzzle
+	return (newPuzzle, cost)
 
 def moveBlankLeft(p):
+	cost = 1
 	blankIndex = getBlankIndex(p)
 	if (blankIndex % 3) == 0:
-		return p
+		return (p,cost)
 	blankIndex = getBlankIndex(p)
 	newPuzzle = p.copy()
 	newPuzzle[blankIndex] = newPuzzle[blankIndex-1]
 	newPuzzle[blankIndex-1] = 0
-	return newPuzzle
+	return (newPuzzle, cost)
 
-
+#checks if current puzzle state is the goal state
 def isGoalState(p):
-	if p == [1,2,3,4,5,6,7,8,0]:
+	if p == goal:
 		return True
 	return False
 
-#takes a node of form (value, tiebreaker value, puzzle state) and returns list of puzzle states (not nodes) for each operator
+#takes a node of form (value, tiebreaker value, puzzle state) and returns list of tuples of (puzzle states, cost of operator) -- these tuples are not the same type of tuples used in queue
 def expand(node):
-	expansion = [moveBlankUp(node[2]),moveBlankDown(node[2]),moveBlankRight(node[2]),moveBlankLeft(node[2])]
+	expansion = [ moveBlankUp(node[2]), moveBlankDown(node[2]), moveBlankRight(node[2]), moveBlankLeft(node[2])]
 	return expansion
 
+#takes queue of nodes, list of nodes that were just expanded, and set of visited states
+#if states in expansion have never been visited, enqueue them based on operator cost and return new queue
+#may not be needed, due to aStar function
 def uniformCost(nodes, expansion, visited):
 	for state in expansion:
-		if tuple(state) not in visited:
-			nodes.put( (1, next(tiebreaker), state) )
+		if tuple(state[0]) not in visited:
+			nodes.put( (state[1], next(tiebreaker), state[0]) )
 
 	return nodes
 
 #counts number of tiles not in the correct position, excluding blank (0)
+#may not be needed, due to aStar function
 def misplacedTile(nodes, expansion, visited): 
 	for state in expansion:
 		numMisplaced = 0
-		for i in range(len(state)):
-			if state[i] != i+1:
-				if state[i] != 0:
+		for i in range(len(state[0])):
+			if (state[0])[i] != i+1:
+				if (state[0])[i] != 0:
 					numMisplaced += 1
 		if tuple(state) not in visited:
-			nodes.put( (numMisplaced, next(tiebreaker), state) )
+			nodes.put( (numMisplaced, next(tiebreaker), state[0]) )
+
+	return nodes
+#counts number of tiles not in the correct position, excluding blank (0)
+#returns number of misplaced tiles for aStar to use as heuristic
+def mt(p):
+	numMisplaced = 0
+	for i in range(len(p)):
+		if p[i] != i+1:
+			if p[i] != 0:
+				numMisplaced += 1
+	return numMisplaced
+	
+#this code was found at the following URL:
+#https://stackoverflow.com/questions/39759721/calculating-the-manhattan-distance-in-the-eight-puzzle
+#may not be needed due to aStar function
+def manhattanDist(nodes, expansion, visited):
+	for state in expansion:
+		if tuple(state[0]) not in visited:
+			manDist = sum(abs((val-1)%3 - i%3) + abs((val-1)//3 - i//3)
+				for i, val in enumerate(state[0]) if val)
+			nodes.put( (manDist, next(tiebreaker), state[0]) )
 
 	return nodes
 
 #this code was found at the following URL:
 #https://stackoverflow.com/questions/39759721/calculating-the-manhattan-distance-in-the-eight-puzzle
-def manhattanDist(nodes, expansion, visited):
-	for state in expansion:
-		if tuple(state) not in visited:
-			manDist = sum(abs((val-1)%3 - i%3) + abs((val-1)//3 - i//3)
-				for i, val in enumerate(state) if val)
-			nodes.put( (manDist, next(tiebreaker), state) )
+#returns sum of manhattan distances for each puzzle piece for aStar to use as heuristic
+def md(p):
+	manDist = sum(abs((val-1)%3 - i%3) + abs((val-1)//3 - i//3)
+		for i, val in enumerate(p) if val)
+	return manDist
 
+#this search algorithm encompasses all three options
+def aStar(nodes, expansion, visited, choice):
+	#A* combines cost to reach this state with heuristic evaluation so g(n) + h(n)
+	#with h(n) = 0 then it reduces to uniform cost search
+	hn = 0
+	#look at all new states and generate priority number based on user selected algorithms and enqueue node for that state
+	#if user selects UCS then priority number is determined in operator functions
+	for state in expansion:
+		if tuple(state[0]) not in visited:
+			gn = state[1]
+			if choice == "2":
+				hn = mt(state[0])
+			elif choice == "3":
+				hn = md(state[0])
+			nodes.put( (gn + hn, next(tiebreaker), state[0]) )
 	return nodes
 
-
-def generalSearch(p, queueingFunc):
+def generalSearch(p, choice):
 	#a queue for nodes to expand
 	nodes = queue.PriorityQueue()
 	#priority queue containing elements of the pattern (priority_number, tiebreaker, puzzle)
@@ -156,18 +206,18 @@ def generalSearch(p, queueingFunc):
 		printPuzzle(node[2])
 		#mark this puzzle state as visited
 		visited.add(tuple(node[2]))
-		#if we've found goal state, return it
+		#if we've found goal state, notify user and return
 		if isGoalState(node[2]):
-			print("i did it")
-			print(node[2])
+			print("Puzzle solved:")
+			printPuzzle(node[2])
 			return
-		print("not goal\n\n")
-		#if there's more nodes and we haven't found goal yet, expand curr node and enqueue using queueing function
-		nodes = queueingFunc(nodes, expand(node), visited)
+		print("This state is not the goal state\n\n")
+		#if there's more nodes and we haven't found goal yet, expand curr node and enqueue using aStar algorithm
+		nodes = aStar(nodes, expand(node), visited, choice)
 
 def printPuzzle(puzzle):
 	for i in range(len(puzzle)):
-		if i % 3 == 0:
+		if i % 3 == 0 and i > 0:
 			print()
 		print(str(puzzle[i]) + " ", end='')
 	print('\n')
